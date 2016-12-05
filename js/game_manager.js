e99da02313757dd440f142a319c018a35896dd85
Jukenbuken <spacebub345@gmail.com>
2016-12-05 16:26:31 +0000

    50 functionality

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.75 ? 2 : Math.random() < 0.5 ? 4 : 0;
+    var value = Math.random() < 0.8 ? 5 : Math.random() < 10 ?  : 15;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -128,9 +128,8 @@ GameManager.prototype.move = function (direction) {
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom || next && next.value == 0 || next && tile.value == 0) {
           
-	        var merged = new Tile(positions.next, tile.value * 2);
-          if(next && next.value == 0) merged.value = tile.value / 2;
-          if(next && tile.value == 0) merged.value = next.value / 2;
+	        var merged = new Tile(positions.next, tile.value + 5);
+       
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -142,8 +141,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 50 tile
+          if (merged.value === 50) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
